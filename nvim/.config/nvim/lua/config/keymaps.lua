20acf45be7acc54157e5a8b9b1c3af4bdf7a7fca
typedcode <bug@typedcode.de>
2025-04-15 21:55:46 +0200

    adding first part for nvim configuration

---
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,3 @@
+vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", {desc="Open Parent directory in Oil"})
+
+vim.keymap.set("n", "<leader>pv", "vim.cmd.Ex", {desc="Opens the netrw file manager"})
---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,34 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+
+vim.opt.smarttab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+vim.opt.breakindent = true
+
+vim.opt.cursorline = true
+
+vim.opt.undofile = true
+
+vim.opt.mouse = "a"
+
+vim.opt.showmode = false
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.signcolumn = "yes"
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+vim.opt.scrolloff = 10
