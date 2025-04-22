bfa2f229d50bc341f92fa31bcbfa34a2beb7f684
typedcode <bug@typedcode.de>
2025-04-23 00:11:27 +0200

    adding lspconfig

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,2 @@
 vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", {desc="Open Parent directory in Oil"})
-
-vim.keymap.set("n", "<leader>pv", "vim.cmd.Ex", {desc="Opens the netrw file manager"})
+vim.keymap.set("n", "gl", function() vim.diagnostic.open_float() end, {desc="Open Diagnostics in float"})
